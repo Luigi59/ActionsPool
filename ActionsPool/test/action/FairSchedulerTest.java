ae5064ede03f10954d0f18ba48b569c5b688ad01
Estelle <Estelle@Estelle-HP>
2015-10-30 22:56:44 +0100

    ajout tests

---
+++ ActionsPool/test/action/FairSchedulerTest.java
@@ -0,0 +1,22 @@
+package action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+import action.FairScheduler;
+import action.Scheduler;
+
+public class FairSchedulerTest extends SchedulerTest {
+
+	protected Scheduler createScheduler() {
+		return new FairScheduler(0);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
