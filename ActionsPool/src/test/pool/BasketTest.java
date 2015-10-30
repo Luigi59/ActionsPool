12f53416d0c224921d1606f341977b89bbffb842
Estelle <Estelle@Estelle-HP>
2015-10-30 18:26:40 +0100

    resource test

---
+++ ActionsPool/src/test/pool/BasketTest.java
@@ -0,0 +1,20 @@
+package test.pool;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pool.Basket;
+
+public class BasketTest {
+	
+	private Basket b ;
+	
+	@Test
+	public void testDescription() {
+		this.b = new Basket();
+		assertEquals("This is a basket",b.description());
+		
+	}
+
+}
