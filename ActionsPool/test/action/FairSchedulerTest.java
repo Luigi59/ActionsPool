3ff5de00b8c1dd4d8b6c1454af73f31096341173
Estelle <Estelle@Estelle-HP>
2015-10-31 14:53:03 +0100

    ajout test

--- ActionsPool/test/action/FairSchedulerTest.java
+++ ActionsPool/test/action/FairSchedulerTest.java
@@ -4,19 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 import action.FairScheduler;
 import action.Scheduler;
 
 public class FairSchedulerTest extends SchedulerTest {
-
+	
 	protected Scheduler createScheduler() {
-		return new FairScheduler(0);
+		FairScheduler s = new FairScheduler();
+		s.addAction(new Dress(1));
+		s.addAction(new Swim(2));
+		return s;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsReady() {
+		assertTrue(super.a.isReady());
+		super.a.doStep();
+		assertFalse(super.a.isReady());
+	}
+	
+	@Test
+	public void testIsInProgress() {
+		assertFalse(super.a.isInProgress());
+		super.a.doStep();
+		assertTrue(super.a.isInProgress());
 	}
 
+	@Test
+	public void testIsFinished() {
+		assertFalse(super.a.isFinished());
+		super.a.doStep();
+		super.a.doStep();
+		super.a.doStep();
+		assertTrue(super.a.isFinished());
+		
+		
+		
+	}
 }
